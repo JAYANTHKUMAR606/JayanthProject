62f31a08581e60684732e6a728343bcf0464a993
JAYANTHKUMAR606 <suryajayanthkumar606@gmail.com>
2024-08-13 01:03:12 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,18 @@
+#create a dictionary with student data
+import pandas as pd
+student_data ={
+    "name":     ["akash","bala","cherry","dam","evoke","fresh"],
+    "age":      [20,21,19,22,23,20],
+    "major":    ["cs","math","phy","eco","bio","chem"],
+    "score":    [85,92,78,95,88,90],
+    "twon":     ["hyd","rr","wnp","ngknl","knl","gdwl"],
+    "pass out": [22,23,24,25,21,25],
+    "fee remeb":[False,True,False,True,False,True]
+
+}
+#Create a data frame from the dictionary
+df = pd.DataFrame(student_data)
+#print the data frame
+print(df)
+#disp the information about data frame
+print(df.info)
---
+++ task3.py
@@ -0,0 +1,7 @@
+#creating data frame
+import pandas as pd
+student_register =pd.DataFrame()
+student_register['name']=['abhi', 'ram', 'akash', 'sam']
+student_register['age']=['20', '21', '19', '23']
+student_register['student']=['false', 'true', 'false', 'true']
+print(student_register)
